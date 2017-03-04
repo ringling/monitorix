7c8bbce7a5a8dcc92851125e6aa9174410a13bf2
Thomas Ringling <thomas.ringling@gmail.com>
2017-03-04 09:49:14 +0100

    Handle redirects and return body

--- lib/pinger.ex
+++ lib/pinger.ex
@@ -1,21 +1,21 @@
 defmodule Pinger do
 
-  def ping(url, httpoison \\ HTTPoison) do
-    {ping_time, value}  = :timer.tc(fn -> Pinger.execute(url, httpoison) end, [])
+  def ping(url, follow_redirect, httpoison \\ HTTPoison) do
+    {ping_time, value}  = :timer.tc(fn -> Pinger.execute(url, follow_redirect, httpoison) end, [])
     value |> Map.put(:time, "#{ping_time/1000}ms")
   end
 
-  def execute(url, httpoison) do
+  def execute(url, follow_redirect, httpoison) do
 
-    case httpoison.get(url) do
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
-        %{status: 200}
+    case httpoison.get(url, [], [follow_redirect: follow_redirect]) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        %{status: 200, body: body}
 
-      {:ok, %HTTPoison.Response{status_code: 301}} ->
-        %{status: 301}
+      {:ok, %HTTPoison.Response{status_code: 301, body: body}} ->
+        %{status: 301, body: body}
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        %{status: 404}
+      {:ok, %HTTPoison.Response{status_code: 404, body: body}} ->
+        %{status: 404, body: body}
 
       {:ok, %HTTPoison.Response{status_code: 500}} ->
         %{status: 500}
--- test/lib/monitorix/pinger_test.exs
+++ test/lib/monitorix/pinger_test.exs
@@ -1,16 +1,33 @@
 defmodule PingerTest do
   use ExUnit.Case
 
+  @httpoison_mock HTTPoisonMock
+  #@httpoison_mock HTTPoison
+
   test "should ping site" do
     url = "http://web1.privacytree.eu/1"
-    %{status: status, time: time} = Pinger.ping(url, HTTPoisonMock)
+    %{status: status, time: time, body: body} = Pinger.ping(url, false, @httpoison_mock)
     assert status == 200
+    assert time
+  end
+
+  test "should follow redirects" do
+    url = "http://www.privacytree.eu/da"
+    %{status: status, time: time, body: body} = Pinger.ping(url, true, @httpoison_mock)
+    assert status == 200
+    assert time
+  end
+
+  test "should not follow redirects" do
+    url = "http://www.privacytree.eu/da"
+    %{status: status, time: time, body: body} = Pinger.ping(url, false, @httpoison_mock)
+    assert status == 301
     assert time
   end
 
   test "should return error" do
     url = "https://web1.privacytree.eu"
-    %{error: error, time: time} = Pinger.ping(url, HTTPoisonMock)
+    %{error: error, time: time} = Pinger.ping(url, false, @httpoison_mock)
     assert error == :econnrefused
     assert time
   end
@@ -19,11 +36,20 @@ end
 
 defmodule HTTPoisonMock do
 
-  def get("http://web1.privacytree.eu/1") do
-    {:ok, %HTTPoison.Response{status_code: 200}}
+
+  def get("http://www.privacytree.eu/da", [], [follow_redirect: false]) do
+    {:ok, %HTTPoison.Response{status_code: 301, body: "body"}}
   end
 
-  def get("https://web1.privacytree.eu") do
+  def get("http://www.privacytree.eu/da", [], [follow_redirect: true]) do
+    {:ok, %HTTPoison.Response{status_code: 200, body: "body"}}
+  end
+
+  def get("http://web1.privacytree.eu/1", [], _) do
+    {:ok, %HTTPoison.Response{status_code: 200, body: "body"}}
+  end
+
+  def get("https://web1.privacytree.eu", [], _) do
     {:error, %HTTPoison.Error{reason: :econnrefused}}
   end
 
